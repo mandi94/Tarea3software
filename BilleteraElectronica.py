1d54ac88d38dfbfeb884ff40c985c129adbd7f34
lautarovc <casper1994@gmail.com>
2017-10-18 19:28:12 -0400

    Add files via upload

--- BilleteraElectronica.py
+++ BilleteraElectronica.py
@@ -9,71 +9,145 @@
 
 
 class BilleteraElectronica:
-  
+
     def __init__(self, identif, nombres, apellidos, CI, PIN):
         self.id=identif
-        self.saldoInicial=0
         self.nombres=nombres
         self.apellidos=apellidos
         self.CI=CI
         self.PIN=PIN
 
+        self.creditos = self.Creditos()
+        self.debitos = self.Debitos()
 
-	#######################
-	#Estructura de Credito#
-	#######################
-    class Credito:
+    #######################
+    #Estructura de Credito#
+    #######################
+    class Creditos:
         # Estructura para guardar cosas relacionadas con la billetera
-        def __init__(self, monto, fecha, identif):
-            self.monto=monto
-            self.fecha= fecha
-            self.id= identif
-      
-    class Debito:
-	#   Estructura para registrar los consumos o debitos de la billetera 
-        def __init__(self, monto, fecha, identif):
-		    self.monto=monto
-		    self.fecha= fecha
-		    self.id= identif
+        def __init__(self):
+            self.creditos = []
 
+        def agregarCredito(self, monto, fecha, identif):
+            self.creditos.append([monto, fecha, identif])
+            return True
+
+        def calcularTotal(self):
+            total = 0
+
+            if (len(self.creditos) == 0):
+                return total
+
+            for credit in self.creditos:
+                total += credit[0]
+
+            return total
+
+      
+    class Debitos:
+    #   Estructura para registrar los consumos o debitos de la billetera 
+        def __init__(self):
+            self.debitos = []
+#######################
+    #Estructura de Credito#
+    #######################
+    class Creditos:
+        # Estructura para guardar cosas relacionadas con la billetera
+        def __init__(self):
+            self.creditos = []
+
+        def agregarCredito(self, monto, fecha, identif):
+            self.creditos.append([monto, fecha, identif])
+            return True
+
+        def calcularTotal(self):
+            total = 0
+
+            if (len(self.creditos) == 0):
+                return total
+
+            for credit in self.creditos:
+                total += credit[0]
+
+            return total
+
+      
+    class Debitos:
+    #   Estructura para registrar los consumos o debitos de la billetera 
+        def __init__(self):
+            self.debitos = []
+
+
+        def agregarDebito(self, monto, fecha, identif):
+            self.debitos.append([monto, fecha, identif])
+            return True
+
+        def calcularTotal(self):
+            total = 0
+
+            if (len(self.debitos) == 0):
+                return total
+
+            for debit in self.debitos:
+                total += debit[0]
+
+            return total
+
+        def agregarDebito(self, monto, fecha, identif):
+            self.debitos.append([monto, fecha, identif])
+            return True
+
+        def calcularTotal(self):
+            total = 0
+
+            if (len(self.debitos) == 0):
+                return total
+
+            for debit in self.debitos:
+                total += debit[0]
+
+            return total
 
     ####################################
-	# Funciones saldo,recarga y consumo#
-	####################################
+    # Funciones saldo,recarga y consumo#
+    ####################################
     
     def saldo(self):
-		print (self.saldoInicial)
-		return self.saldoInicial
+        saldo = self.creditos.calcularTotal() - self.debitos.calcularTotal()
 
-    def recarga(self, monto, fecha, identif, CI):
-        if CI== self.CI:
-            if monto>0:
-                self.Credito(monto, fecha, identif)
-                self.saldoInicial= self.saldoInicial + monto
-                return True
-            else:
-                print("El saldo no puede ser negativo")
-                return False
-        else:
-            print("USUARIO INCORRECTO")
+        return saldo
+
+    def recarga(self, monto, fecha, identif):
+
+        try:
+            assert(monto>0)
+        except:
+            print("No se puede recargar un monto nulo o negativo.")
             return False
 
 
-    
-    def consumo(self, monto, fecha, identif, CI, PIN):
-        if CI==self.CI and PIN==self.PIN:
-            if monto<0:
-                print("El monto de consumo no puede ser negativo")
-                return False
-            if self.saldoInicial>=monto:
-                self.Debito(monto, fecha, identif)
-                self.saldoInicial= self.saldoInicial - monto
-                return True
-            else:
-                print("Saldo insuficiente")
-                return False
-        else:
+        self.creditos.agregarCredito(monto, fecha, identif)
+        return True
+              
+    def consumo(self, monto, fecha, identif, PIN):
+
+        try:
+            assert(PIN==self.PIN)
+
+        except:
             print("DATOS INCORRECTOS")
             return False
+
+        if monto<0:
+            print("El monto de consumo no puede ser negativo")
+            return False
+
+        if self.saldo() >= monto:
+            self.debitos.agregarDebito(monto, fecha, identif)
+            return True
+        else:
+            print("Saldo insuficiente")
+            return False
+
 
             
